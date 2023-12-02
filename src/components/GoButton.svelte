3c2049fe6a71ea8658e57f89dc60ec39f5ab3c9b
Dan <daniel.gomme@yahoo.co.uk>
2023-12-02 20:36:58 +0000

    Add the GoButton component
    This could probably be a component for the confirm AND deny things

---
+++ src/components/GoButton.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let text = "Test";
+</script>
+
+<style lang="scss">
+    .go-button {
+        background-color: forestgreen;
+        padding: 5px 10px 5px 10px;
+        transition: filter 200ms;
+    }
+    
+    .go-button:hover {
+        filter: brightness(115%);
+    }
+</style>
+
+<button class="go-button">{text}</button>
